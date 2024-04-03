38909cecf9982a7cf19fb7ebad9c69cdc5380900
zhangzhengyang <1074385735@qq.com>
2024-04-03 23:26:19 +0800

    安装 sass 并配置样式变量文件

---
+++ 1-vite-project/src/components/Header/index.tsx
@@ -0,0 +1,5 @@
+import "./index.scss"
+
+export default function Header() {
+  return <p className="header">this is Header</p>
+}
--- 1-vite-project/vite.config.ts
+++ 1-vite-project/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, normalizePath } from "vite"
+import react from "@vitejs/plugin-react"
+
+// 如果类型报错，需要安装 @types/node: pnpm i @types/node -D
+import path from "path"
+
+// 全局 scss 文件的路径
+// 用 normalizePath 解决 window 下的路径问题
+
+const variablePath = normalizePath(path.resolve("./src/styles/variables.scss"))
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // css 相关的配置
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // additionalData 的内容会在每个 scss 文件的开头自动注入
+        additionalData: `@import "${variablePath}";`
+      }
+    }
+  }
 })
