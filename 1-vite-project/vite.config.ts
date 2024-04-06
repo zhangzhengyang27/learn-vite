9d82485367e01fababe593d40c2f4b0123ff9921
zhangzhengyang <1074385735@qq.com>
2024-04-06 18:46:40 +0800

    添加 autoprefixer 插件

--- 1-vite-project/vite.config.ts
+++ 1-vite-project/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, normalizePath } from "vite"
 import react from "@vitejs/plugin-react"
+import autoprefixer from "autoprefixer"
 
 // 如果类型报错，需要安装 @types/node: pnpm i @types/node -D
 import path from "path"
@@ -19,6 +20,14 @@ export default defineConfig({
         // additionalData 的内容会在每个 scss 文件的开头自动注入
         additionalData: `@import "${variablePath}";`
       }
+    },
+    //进行 PostCSS 配置
+    postcss: {
+      plugins: [
+        autoprefixer({
+          overrideBrowserslist: ["Chrome > 40", "ff > 31", "ie 11"] // 指定目标浏览器
+        })
+      ]
     }
   }
 })
