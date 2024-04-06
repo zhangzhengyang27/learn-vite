a07e06b2fabab5a43b73c515d63d0ee89ef4bdf0
zhangzhengyang <1074385735@qq.com>
2024-04-06 18:36:57 +0800

    使用 css modules 处理样式

--- 1-vite-project/src/components/Header/index.tsx
+++ 1-vite-project/src/components/Header/index.tsx
@@ -1,5 +1,11 @@
 import "./index.scss"
+import styles from "./index.module.scss"
 
 export default function Header() {
-  return <p className="header">this is Header</p>
+  return (
+    <>
+      <p className="header">使用 scss 处理处理样式</p>
+      <p className={styles.header}>使用 CSS Modules 处理处理样式</p>
+    </>
+  )
 }
