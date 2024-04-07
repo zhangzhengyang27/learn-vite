8073edf5dc3e2909347c14c145155ceb7a871c31
zhangzhengyang <1074385735@qq.com>
2024-04-07 15:08:15 +0800

    开发 esbuild 插件

---
+++ 03-plugin-esbuild/src/index.jsx
@@ -0,0 +1,8 @@
+// src/index.jsx
+
+import { render } from "react-dom";
+import React from "react";
+
+let Greet = () => <h1>Hello, xiaoye!</h1>;
+
+render(<Greet />, document.getElementById("root"));
